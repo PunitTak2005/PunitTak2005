2f8dcdf5011b1a0e6a4fee062f761380553a352c
PunitTak2005 <147526916+PunitTak2005@users.noreply.github.com>
2024-05-22 21:03:06 +0530

    Add files via upload

---
+++ count.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Counting from 1 to 5 using a for loop:\n");
+
+    for (int count = 1; count <= 5; count++) {
+
+        printf("%d\n", count);
+
+    }
+
+    return 0;
+
+}
