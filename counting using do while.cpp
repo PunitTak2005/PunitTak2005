e0982bb405fb4c1b97a2045a4b97e620711c60cf
PunitTak2005 <147526916+PunitTak2005@users.noreply.github.com>
2024-05-22 20:13:03 +0530

    Add files via upload

---
+++ counting using do while.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int count = 1;
+
+    printf("Counting from 1 to 5 using a while loop:\n");
+
+    while (count <= 5) {
+
+        printf("%d\n", count);
+
+        count++;
+
+    }
+
+    return 0;
+
+}
