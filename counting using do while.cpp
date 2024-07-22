4937f3ad237626b11d5aeddc45f1a4a01360ee59
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-07-22 11:01:22 +0530

    Update counting using do while.cpp

--- counting using do while.cpp
+++ counting using do while.cpp
@@ -1,19 +1,10 @@
-#include <stdio.h>
-
 int main() {
-
     int count = 1;
-
-    printf("Counting from 1 to 5 using a while loop:\n");
-
-    while (count <= 5) {
-
-        printf("%d\n", count);
-
+    do {
+        printf("%d ", count);
         count++;
-
-    }
-
+    } while (count <= 10);
+    printf("\n");
     return 0;
-
 }
+
