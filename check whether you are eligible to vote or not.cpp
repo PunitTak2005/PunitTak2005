8d7ee20159dea2ec6a47a0b48514b26574d94bbb
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2024-10-10 18:12:38 +0530

    Update check whether you are eligible to vote or not.cpp

--- check whether you are eligible to vote or not.cpp
+++ check whether you are eligible to vote or not.cpp
@@ -1,29 +1,27 @@
 #include <stdio.h>
+
 int main()
 {
-	int age;
-	char gender;
-	printf("Enter your age");
-	scanf("%d",&age);
-	printf("Enter your gender (M/F)");
-	scanf(" %c",&gender);
-	
-	
-	if (age >= 18){
-		if ( gender == 'M' || gender == 'm'){
-		
-		
-		printf("You are eligible for men's category.\n "); }
-		else if ( gender == 'f'  ||  gender == 'F'){
-			printf ( "you are eligible for women's category. \n");
-		}
-		else{printf("invalid input");
-	}
-	
-		}else{printf("Not eligible");
-		
-		}return 0 ;
-		
-		}
-	
+    int age;
+    char gender;
 
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    printf("Enter your gender (M/F): ");
+    scanf(" %c", &gender);
+
+    if (age >= 18) {
+        if (gender == 'M' || gender == 'm') {
+            printf("You are eligible for the men's category.\n");
+        } else if (gender == 'F' || gender == 'f') {
+            printf("You are eligible for the women's category.\n");
+        } else {
+            printf("Invalid input.\n");
+        }
+    } else {
+        printf("Not eligible.\n");
+    }
+
+    return 0;
+}
